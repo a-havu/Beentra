eb3e6621e7df5248b64eafd55e11657b6b46438f
mohdkhlouf <mohammad.khlouf@gmail.com>
2026-01-19 16:07:51 +0200

    adding menu

--- ft_transcendence/components/main/Footer.tsx
+++ ft_transcendence/components/main/Footer.tsx
@@ -1,7 +1,10 @@
+import Menu from "./Menu";
+
 export default function Footer() {
   return (
     <div>
       <h3> Footer</h3>
+      <Menu />
     </div>
   );
 }
---
+++ ft_transcendence/components/main/Menu.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Menu() {
+  return (
+    <ul>
+      <li>
+        <Link href="/terms">Terms</Link>
+      </li>
+    </ul>
+  );
+}
