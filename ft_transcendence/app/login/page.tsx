6d8e225db2ebe01e8aa44891cf75b6755a7e83b5
mohdkhlouf <mohammad.khlouf@gmail.com>
2026-01-21 16:00:41 +0200

    uploading files

--- ft_transcendence/app/login/page.tsx
+++ ft_transcendence/app/login/page.tsx
@@ -1,11 +1,15 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
 import { LoginForm } from "@/components/login/LoginForm"
+import { prisma } from "@/lib/prisma"
 
 export const metadata ={
   title:'login page'
 }
-export default function LoginPage() {
+export default async function LoginPage() {
+
+  const users = await prisma.user.findMany()
+  console.log("users:",users)
   return (
             <div className="flex flex-1 w-full items-center justify-center">
           <div className="w-full max-w-xs">
---
+++ ft_transcendence/prisma/migrations/20260121121743_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Page" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Page_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
