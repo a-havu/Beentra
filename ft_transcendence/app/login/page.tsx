cc5bd72eca3b09bd13e6a8b90094c756075a9c32
Mohammad <mohammad.khlouf@gmail.com>
2026-01-29 08:24:46 +0200

    removing extra files

--- ft_transcendence/app/login/page.tsx
+++ ft_transcendence/app/login/page.tsx
@@ -1,15 +1,10 @@
 import { GalleryVerticalEnd } from "lucide-react"
-
 import { LoginForm } from "@/components/login/LoginForm"
-import { prisma } from "@/lib/prisma"
 
 export const metadata ={
   title:'login page'
 }
 export default async function LoginPage() {
-
-  const users = await prisma.user.findMany()
-  console.log("users:",users)
   return (
             <div className="flex flex-1 w-full items-center justify-center">
           <div className="w-full max-w-xs">
