d77bf36294b2973fc1a5ed243df89028738b92fc
mohdkhlouf <mohammad.khlouf@gmail.com>
2026-02-05 11:24:01 +0200

    adding styles

--- ft_transcendence/app/(protected)/dashboard/page.tsx
+++ ft_transcendence/app/(protected)/dashboard/page.tsx
@@ -18,9 +18,11 @@ export default async function Home() {
   return (
     <div>
       <h3>Welcome, {session.userEmail}, you are {session.role}!</h3>
+      
       <h3> Dashboard Page</h3>
-      <Link href="/infoPages">manage pages</Link>
-      <></>
+      <div className="m-2">
+        <Link className ="px-4 py-2 bg-blue-600 text-white rounded m" href="/infoPages">manage pages</Link>
+      </div>
     </div>
   );
 }
