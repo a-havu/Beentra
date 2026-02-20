4086b1f98af34b8eb436f2f97ca5c0bb57eadb67
Roni Eerikainen <reerikai@c1r5p19.hive.fi>
2026-02-20 15:43:38 +0200

    Functional buttons testing

--- ft_transcendence/components/ui/Button.tsx
+++ ft_transcendence/components/ui/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
   children: React.ReactNode;  					// The text/content inside the button
   onClick?: () => void;							// Function call when clicked, optional because of the '?'
   type?: "button" | "submit" | "reset";			// The type of the button
-  variant?: "primary" | "secondary" | "sidebar" | "delete" | "adding";
+  variant?: "primary" | "secondary" | "sidebar" | "delete" | "adding" | "edit";
   size?: "small" | "medium" | "large";
 };
 
@@ -21,7 +21,8 @@ export function Button({
 		secondary: "bg-gray-600 hover:bg-gray-700 text-white",
 		sidebar: "w-full text-center bg-white border-2 border-gray-300 rounded-lg text-lg font-bold text-gray-900 hover:bg-gray-900 hover:border-white hover:text-white transition",
 		delete: "bg-red-600 hover:bg-red-700 text-white",
-		adding: "bg-green-600 hover:bg-green-700 text-white"
+		adding: "bg-green-600 hover:bg-green-700 text-white",
+		edit: "bg-yellow-500 hover:bg-yellow-600 text-white"
 	};
 
 	const sizeStyles = {
