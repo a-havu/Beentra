40b9ff0084789a640116865d02fee5038d287950
mohdkhlouf <mohammad.khlouf@gmail.com>
2026-03-02 15:27:43 +0200

    generating the model and creating the api call

--- ft_transcendence/app/api/apikey/route.ts
+++ ft_transcendence/app/api/apikey/route.ts
@@ -1,8 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import { prisma } from "@/lib/prisma";
 
 
 export async function POST(request:NextRequest){
-    const {userEmail} = await request.json()
+    try{
 
+        const {userEmail} = await request.json()
+    const hashedKey = await bcrypt.hash(userEmail,10)
+
+    const newKey = await prisma.apikey.create({
+        data:{
+            email:userEmail,
+            key : hashedKey
+        }
+    }
+        )
     return NextResponse.json({success:true},{status:200})
+    }catch(e){
+        
+    }
+    
 }
