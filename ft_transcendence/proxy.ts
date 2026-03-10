469c79ef6507e94d5929f0f8298237480f3e91d7
Mohammad <mohammad.khlouf@gmail.com>
2026-03-11 00:05:38 +0200

    making default layout without header and footer

--- ft_transcendence/proxy.ts
+++ ft_transcendence/proxy.ts
@@ -150,7 +150,7 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.png|.*\\.jpg|.*\\.svg|.*\\.ico).*)",
   ],
 };
 
