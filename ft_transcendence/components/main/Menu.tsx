1e0fcddf514cf987e65d3deebf43fa902656d651
hahamdan <hagerabuanga@gmail.com>
2026-03-12 21:50:42 +0200

    Fix header

--- ft_transcendence/components/main/Menu.tsx
+++ ft_transcendence/components/main/Menu.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Menu() {
   return (
-    <ul className="flex w-full md:w-auto flex-col md:flex-row gap-4 justify-center items-center">
+    <ul className="flex flex-row gap-4 items-center">
       <li>
         <Link href="/projects">Projects</Link>
       </li>
