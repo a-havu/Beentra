49d0e38d0887634989fdfe7815cb2fe5aa5ab6c7
Mohammad <mohammad.khlouf@gmail.com>
2026-03-21 12:58:58 +0200

    adding friends page

---
+++ ft_transcendence/app/(public)/users/[userId]/friends/page.tsx
@@ -0,0 +1,12 @@
+import FriendsSection from "@/components/profile/FriendsSection"
+
+export default function friendsPage() {
+
+
+  return (
+    <>
+      <h2>Friends page</h2>
+      <FriendsSection />
+    </>
+  )
+}
