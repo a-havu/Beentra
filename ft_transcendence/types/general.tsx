044d3c35b65566f476d3c7f5fa4f553492ee1dc3
mohdkhlouf <mohammad.khlouf@gmail.com>
2026-03-24 11:33:05 +0200

    fix type error in editproject.tsx

--- ft_transcendence/types/general.tsx
+++ ft_transcendence/types/general.tsx
@@ -1,15 +1,15 @@
 export type session_data = {
-  userId: string,
-  email: string,
-  role: number,
-  avatar_url: string | null
-}
+  userId: string;
+  email: string;
+  role: number;
+  avatar_url: string | null;
+};
 
 export type PageData = {
-  pageTitle: string,
-  pageText: string,
-  authorId: number
-}
+  pageTitle: string;
+  pageText: string;
+  authorId: number;
+};
 
 export type EventData = {
   id: string;
@@ -26,4 +26,19 @@ export type EventData = {
   maxSpots: number;
   subscriberCount: number;
   isSubscribed: boolean;
+};
+
+type LocalProject = {
+  id: string;
+  projectName: string;
+  oneLiner: string;
+  link: string | null;
+  techStack: string | null;
+  description: string | null;
+  createdAt: Date;
+  creatorId: string | null;
+  image: string | null;
+  imageKitId: string | null;
+  imagekitFileId: string | null;
+  creator: { id: string; username: string; fullName: string | null } | null;
 };
