fcb93aa5b8dec662183a89513a77e42a84038901
mohdkhlouf <mohammad.khlouf@gmail.com>
2026-03-24 11:22:11 +0200

    fix tyescript errors

--- ft_transcendence/app/reference/route.tsx
+++ ft_transcendence/app/reference/route.tsx
@@ -1,9 +1,9 @@
-import { ApiReference } from '@scalar/nextjs-api-reference'
+import { ApiReference } from "@scalar/nextjs-api-reference";
 
 const config = {
-  url: '/api/openapi.json',
-  theme: 'default', // or 'purple', 'blue', 'green', etc.
-  pageTitle: 'Beentra API Reference',
-}
+  url: "/api/openapi.json",
+  theme: "default" as const,
+  pageTitle: "Beentra API Reference",
+};
 
-export const GET = ApiReference(config)
+export const GET = ApiReference(config);
