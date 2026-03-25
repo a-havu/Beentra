5090a3e5fdb88eff12a5ff442e7e4ca1f6adea79
Mohammad <mohammad.khlouf@gmail.com>
2026-03-26 00:30:27 +0200

    change the imageKit to node version

--- ft_transcendence/app/api/imagekit-auth/route.ts
+++ ft_transcendence/app/api/imagekit-auth/route.ts
@@ -1,12 +1,12 @@
-import ImageKit from "imagekit";
+import crypto from "crypto";
 
 export async function GET() {
-  const imagekit = new ImageKit({
-    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-  });
+  const token = crypto.randomUUID();
+  const expire = Math.floor(Date.now() / 1000) + 2400;
+  const signature = crypto
+    .createHmac("sha1", process.env.IMAGEKIT_PRIVATE_KEY!)
+    .update(token + expire)
+    .digest("hex");
 
-  const auth = imagekit.getAuthenticationParameters();
-  return Response.json(auth);
+  return Response.json({ token, expire, signature });
 }
