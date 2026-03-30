e233ec9f445451eb5e33fad5b32262c7e7fc64e1
ahavu <ahavu@student.hive.fi>
2026-03-30 15:12:44 +0300

    events page styling & mobile adjustments

--- ft_transcendence/components/main/Header.tsx
+++ ft_transcendence/components/main/Header.tsx
@@ -6,14 +6,15 @@ import Image from 'next/image';
 export default async function Header() {
 
   return (
-    <header className="header flex flex-row items-center justify-between p-6 h-16 w-full overflow-visible">
-		<div className="flex flex-row gap-3"><Image
+    <header className="header flex flex-row items-center justify-between md:p-6 h-16 w-full overflow-visible">
+		<div className="flex flex-row gap-3"><Link href="/"><Image
 		src="/favicon.ico"
 		alt="logo"
 		width={32}
 		height={32}
-		/>
-      <h3><Link href="/">Beentra</Link></h3></div>
+		/></Link>
+      <h3 className="hidden md:block"><Link href="/">Beentra</Link></h3>
+	  </div>
       <Menu />
       <LogHeader />
     </header >
