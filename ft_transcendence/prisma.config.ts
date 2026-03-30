30ede14344686fe427ce6491faf7289019bd781a
mohdkhlouf <mohammad.khlouf@gmail.com>
2026-03-30 12:24:55 +0300

    reset user role to user

--- ft_transcendence/prisma.config.ts
+++ ft_transcendence/prisma.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "prisma/config";
+import * as dotenv from "dotenv";
+
+dotenv.config(); // loads .env into process.env
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
