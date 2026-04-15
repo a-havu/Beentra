5dadd1ba090ac01d83df4bcd99c9553fcdbb865e
mohdkhlouf <mohammad.khlouf@gmail.com>
2026-04-15 10:38:16 +0300

    add meta data

--- app/(public)/registration/page.tsx
+++ app/(public)/registration/page.tsx
@@ -1,15 +1,16 @@
-import { RegistrationForm } from "@/components/registration/RegistrationForm"
-export const metadata ={
-  title:'registration'
-}
+import { RegistrationForm } from "@/components/registration/RegistrationForm";
+
+export const metadata = {
+  title: "registration",
+};
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center w-full">
-      <h3 className="text-2xl font-bold text-center mb-4 mt-6">Create an Account</h3>
-      <RegistrationForm
-	  	admin={false}
-	  />
+      <h3 className="text-2xl font-bold text-center mb-4 mt-6">
+        Create an Account
+      </h3>
+      <RegistrationForm admin={false} />
     </div>
   );
 }
--- app/(public)/terms/page.tsx
+++ app/(public)/terms/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 export const dynamic = "force-dynamic";
+
 export const metadata = {
   title: "Terms",
 };
